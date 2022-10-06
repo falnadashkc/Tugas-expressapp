4905f98443aedba43331ab8f88ce85432fb5f364
falnadashkc <113893312+falnadashkc@users.noreply.github.com>
2022-10-06 15:11:44 +0700

    express app

--- expressapp/app.js
+++ expressapp/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./app_server/routes/index');
 var users = require('./app_server/routes/users');
+var admin = require('./app_server/routes/admin');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/admin',admin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
